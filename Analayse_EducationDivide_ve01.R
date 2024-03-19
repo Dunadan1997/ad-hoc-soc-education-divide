85e6978e84896d1bf7c298bcd8608dfa7b6c6725
Bruno Alves de Carvalho <105599594+Dunadan1997@users.noreply.github.com>
2024-03-19 21:32:52 +0100

    Initial commit for the analysis of educational divide

--- Analayse_EducationDivide_ve01.R
+++ Analayse_EducationDivide_ve01.R
@@ -18,15 +18,68 @@ source("R_Scripts/FunctionRepository_20231016_ve01.R")
 
 # Load data
 merged_data_shp <-
-  readRDS("SHP/Data_Aggregated_1999_2022/cached_education_divide.rds")
-education_divide <- 
-  read_csv("/Users/brunoalvesdecarvalho/Desktop/ad_hoc_soc_new_ed_divide.csv", 
-           col_types = list("i","f","i","f","f","f","d")) 
+  readRDS("Cached_Data/cached_education_divide_shp.rds")
 
+merged_data_ess <- 
+  readRDS("Cached_Data/cached_education_divide_ess.rds")
+
+education_divide <- 
+  read_csv("OFS/ad-hoc-soc-new-ed-divide-education-divide-ve02.csv", 
+           col_types = list("i","f","i","f","f","f","f","d")) 
+
+
+# Transform Data ----------------------------------------------------------
+
+merged_data_shp$edulvl_fct_01 <-
+  factor(
+    merged_data_shp$`isced$$`, 
+    levels = c(0,10,20,31,32,33,41,51,52,60), 
+    labels = c(
+      "Not completed primary (compulsory) education", 
+      "Primary or first stage of basic education", 
+      "Lower secondary or Second stage of basic education", 
+      "Upper secondary education (preparation for tertiary education)", 
+      "Upper secondary education (preparation for further prof. education)", 
+      "Upper secondary education (entrance into the labor market)", 
+      "Post-secondary education non tertiary (preparation for an institution for higher education)", 
+      "First stage of tertiary education (general education)", 
+      "First stage of tertiary education (professional education)", 
+      "Second stage of tertiary education"
+      )
+    )
+
+merged_data_shp$edulvl_fct_02 <-
+  fct_collapse(merged_data_shp$edulvl_fct_01,
+               primary = c("Not completed primary (compulsory) education", 
+                           "Primary or first stage of basic education"),
+               secondary = c("Lower secondary or Second stage of basic education", 
+                             "Upper secondary education (preparation for tertiary education)", 
+                             "Upper secondary education (preparation for further prof. education)", 
+                             "Upper secondary education (entrance into the labor market)", 
+                             "Post-secondary education non tertiary (preparation for an institution for higher education)"),
+               tertiary_prfssnl = c("First stage of tertiary education (professional education)"),
+               tertiary_unvrty = c("First stage of tertiary education (general education)",
+                                   "Second stage of tertiary education")
+               )
+
+merged_data_shp$edulvl_fct_02 <-
+  fct_relevel(
+    merged_data_shp$edulvl_fct_02, 
+    c("primary", "secondary", "tertiary_prfssnl", "tertiary_unvrty")
+    )
 
 # Exploratory Data Analysis -----------------------------------------------
 
-education_divide %>% group_by(ed_level_02, decade_of_birth) %>% summarise(sum = sum(prct_of_total, na.rm = T)) %>% ggplot(aes(x = decade_of_birth, y = sum, fill = ed_level_02)) + geom_bar(stat = "identity") + scale_y_continuous(breaks = seq(0, 1, 0.05)) + geom_hline(yintercept = 0.30) + geom_hline(yintercept = 0.5)
+# The threshold for educational divide was reached in the 1990s/2000s, when 25% to 30% of the voting population was university educated
+education_divide %>% 
+  group_by(ed_level_02, decade_of_birth, gender) %>% 
+  summarise(sum = sum(prct_of_total, na.rm = T)) %>% 
+  ggplot(aes(x = decade_of_birth, y = sum, fill = ed_level_02)) + 
+  geom_bar(stat = "identity") + 
+  facet_wrap(~ gender) + 
+  scale_y_continuous(breaks = seq(0, 1, 0.05)) + 
+  geom_hline(yintercept = 0.30) + 
+  geom_hline(yintercept = 0.5)
  
 
 
